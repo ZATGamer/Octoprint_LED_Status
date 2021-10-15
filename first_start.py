725ab364a3b6dda1acae4c5df584015c7eb9c4c6
Anthony <gcnplayer5@gmail.com>
2021-10-15 10:36:32 -0600

    Adding current code to git

---
+++ first_start.py
@@ -0,0 +1,42 @@
+import requests
+import socket
+import configparser
+
+
+def the_hunter():
+    # This will curl every IP in the subnet this machine is in to find the Octoprint Monitor API.
+    # We are assuming this is a /24 subnet because, reasons.
+    # Also assuming you mapped it to port 7070.
+
+    # Get local IP
+    local_ip = socket.gethostbyname(socket.gethostname())
+
+    #Now we will split the IP by octet and build our calls.
+    local_octet = local_ip.split('.')
+
+    x = 1
+    while x <= 255:
+
+        base_ip = "{}.{}.{}.".format(local_octet[0], local_octet[1], local_octet[2])
+        test_ip = "{}{}".format(base_ip, x)
+        if test_ip != local_ip:
+            base_url = 'http://{}:7070/find_me'.format(test_ip)
+            try:
+                print("Trying {}".format(test_ip))
+                r = requests.get(base_url, timeout=.125)
+                if r.status_code == 200:
+                    if r.text == "FOUND ME!":
+                        print("Found It")
+                        config = configparser.ConfigParser()
+                        config['op_monitor'] = {'ipaddress': test_ip}
+                        with open('config.ini', 'w') as configfile:
+                            config.write(configfile)
+                        break
+            except requests.exceptions.ConnectionError:
+                pass
+        x += 1
+
+
+
+if __name__ == '__main__':
+    the_hunter()
---
+++ printer_led_notifier.py
@@ -0,0 +1,136 @@
+import RPi.GPIO as GPIO
+import time
+import requests
+import json
+import configparser
+from os.path import exists
+import first_start
+import datetime
+
+
+red = 5
+green = 6
+blue = 13
+http = 26
+leds = [red, green, blue, http]
+
+
+
+def start_up():
+    # Setup the GPIO Pins
+    GPIO.setwarnings(False)
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+
+    for led in leds:
+        GPIO.setup(led, GPIO.OUT)
+
+    # Flash all LED's
+    for led in leds:
+        GPIO.output(led, GPIO.HIGH)
+    time.sleep(1)
+
+    for led in leds:
+        GPIO.output(led, GPIO.LOW)
+
+
+def led_on(led):
+    GPIO.output(led, GPIO.HIGH)
+
+def led_off(led):
+    GPIO.output(led, GPIO.LOW)
+
+def led_blink(led, last_blink):
+    if last_blink + datetime.timedelta(seconds=1) < datetime.datetime.now():
+        last_blink = datetime.datetime.now()
+        #GPIO.setup(led, GPIO.IN)
+        if GPIO.input(led) == True:
+            #GPIO.setup(led, GPIO.OUT)
+            led_off(led)
+        elif GPIO.input(led) == False:
+            #GPIO.setup(led, GPIO.OUT)
+            led_on(led)
+    return last_blink
+
+
+def http_fail():
+    x = 0
+    while x < 5:
+        for led in leds:
+            led_on(led)
+        time.sleep(.5)
+        for led in leds:
+            led_off(led)
+        time.sleep(.5)
+        x += 1
+
+
+def get_data(ip_address):
+    try:
+        data = requests.get("http://{}:7070/info".format(ip_address), timeout=2).text
+        print(data)
+        led_on(http)
+        data = json.loads(data)
+        stalled = False
+        printing = False
+        idle = False
+
+        for thing in data:
+            printer_status = thing[0]
+            stalled_flag = thing[7]
+            #print(stalled)
+            if printer_status == 'Operational':
+                idle = True
+            if printer_status == 'Printing':
+                printing = True
+            if stalled_flag:
+                stalled = True
+
+
+        if stalled:
+            led_on(red)
+        else:
+            led_off(red)
+
+        if printing:
+            led_on(green)
+        else:
+            led_off(green)
+
+        if idle:
+            led_on(blue)
+        else:
+            led_off(blue)
+
+    except requests.exceptions.ConnectionError:
+        print("HTTP Fail")
+        led_off(http)
+
+
+def get_config():
+    if not exists('config.ini'):
+        first_start.the_hunter()
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    ip_address = config['op_monitor']['ipaddress']
+
+    return ip_address
+
+
+if __name__ == '__main__':
+    last_blink = datetime.datetime.now()
+    last_run = datetime.datetime.now() - datetime.timedelta(seconds=10)
+    start_up()
+    ip_address = get_config()
+
+    while(True):
+        #print("Running Loop")
+        if last_run + datetime.timedelta(seconds=10) <= datetime.datetime.now():
+            print("Running Update")
+            get_data(ip_address)
+            last_run = datetime.datetime.now()
+        time.sleep(.25)
+        #last_blink = led_blink(green, last_blink)
+
